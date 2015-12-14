27ffd27a6379ca7e52e6b7b955c05714952ab4f7
池田享浩 <ikedatakahiro@rrcs-172-254-99-25.nyc.biz.rr.com>
2015-12-15 05:24:05 +0900

    Initial Commit

---
+++ QuizApp2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizApp2
+//
+//  Created by 池田享浩 on 2015/12/15.
+//  Copyright (c) 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
